1e5df62358d3c2ed74a6776a9cc8dc041c75a8eb
Sidharth R K <161910602+sidharth756@users.noreply.github.com>
2026-03-16 20:23:17 +0530

    Add solution: Soltuion using STL LIST in C++

---
+++ Soltuion using STL LIST in C++/Soltuion using STL LIST in C++.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int josephus(int n, int k) {
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = (res + k) % i;
+        }
+        return res + 1;     }
+}
