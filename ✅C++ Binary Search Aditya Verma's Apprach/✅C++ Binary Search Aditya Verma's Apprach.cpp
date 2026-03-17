12e2f5401d08f27975e32a82bfc49d875e1a959e
Sidharth R K <161910602+sidharth756@users.noreply.github.com>
2026-03-17 14:11:23 +0530

    Update solution: ✅C++ | Binary Search | Aditya Verma's Apprach

--- ✅C++ Binary Search Aditya Verma's Apprach/✅C++ Binary Search Aditya Verma's Apprach.cpp
+++ ✅C++ Binary Search Aditya Verma's Apprach/✅C++ Binary Search Aditya Verma's Apprach.cpp
@@ -2,23 +2,21 @@ class Solution {
   public:
     int firstIndex(vector<int> &arr) {
         
-        int low = 0;
-        int mid = 0;
+        int low = 0,mid = 0;
         int high =arr.size();
         if(arr[high-1] ==  0)return -1;
         if(arr[0]==1) return 0;
+        int ans = -1;
+        while (low <= high) { 
         mid = low + (high - low) / 2;
-        while (low <= high) {
-            if (arr[mid] == 1 && arr[mid-1] == 0) {
-                return mid;
-                break;
-            }else if(arr[mid] == 1 && arr[mid - 1] == 1){
-                mid = mid - 1;
+            if (arr[mid] == 1) {
+                ans = mid;
+                high = mid-1;
             }
             else{
-                mid = mid+1;
+                low = mid+1;
             }
         }
-        return -1;
+        return ans;
     }
 };
